4dddbff5b84b180d27e47c57afc79f9b7b9a8a1b
YA LI4428 <YA_LI4428@jabil.com>
2019-11-14 09:37:21 +0800

    t

--- PHPCodeMagic/PHPCodeMagic/templates/codeigniter/empty.controller.php
+++ PHPCodeMagic/PHPCodeMagic/templates/codeigniter/empty.controller.php
@@ -5,7 +5,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * {TABLE} Controller 
  */
 class {TABLE} extends MY_Controller {
+    
+    public function all()
+    {
+        $token = $this->input->post('token');
+        if (empty($token)) {
+            error(400, 'token is null');
+            return;
+        }
 
+        $sign = $this->input->post('sign');
+        if (empty($sign)) {
+            error(400, 'sign is null');
+            return;
+        }
+
+        if (!$this->check_sign($token, $sign)) {
+            error(300, 'sign error');
+            return;
+        }
+
+        $uid = $this->uid($token);
+    }
+    
     public function index() {
 
     }
